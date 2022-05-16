ec857900525ed640638aafcb5a11e75a553195c4
zakuro <z@kuro.red>
2022-05-16 20:19:37 +0900

    fmt

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,9 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-
-
-
 use clap::Parser;
 use karaagecc_source::Source;
 use std::io::{self, Write};
