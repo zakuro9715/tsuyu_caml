b5ad897a9eef4d1d9600c66d35bdfe010581388e
zakuro <z@kuro.red>
2022-05-29 09:58:53 +0900

    add Tokens

--- karaagecc_token/src/lib.rs
+++ karaagecc_token/src/lib.rs
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::iter::Peekable;
+
 use karaagecc_source::Loc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -99,5 +101,62 @@ mod token_tests {
             token!(42, Loc::head()),
             Token::new(TokenKind::IntLiteral(42), Loc::head()),
         )
+    }
+}
+
+pub struct TokenReader<'a> {
+    iter: Peekable<Box<dyn Iterator<Item = Token> + 'a>>,
+}
+
+impl<'a> Iterator for TokenReader<'a> {
+    type Item = Token;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.read()
+    }
+}
+
+impl<'a> TokenReader<'a> {
+    pub fn new(iter: impl Iterator<Item = Token> + 'a) -> Self {
+        Self {
+            iter: (Box::new(iter) as Box<dyn Iterator<Item = Token>>).peekable(),
+        }
+    }
+
+    pub fn read(&mut self) -> Option<Token> {
+        self.iter.next()
+    }
+
+    pub fn peek(&mut self) -> Option<&Token> {
+        self.iter.peek()
+    }
+}
+
+#[cfg(test)]
+mod tokens_tests {
+    use crate::*;
+    use karaage_asserts::*;
+    #[test]
+    fn test_new_and_iter() {
+        let tokens = [
+            token!(1, Loc::head()),
+            token!(2, Loc::head()),
+            token!(3, Loc::head()),
+        ];
+        assert_iter_eq!(TokenReader::new(tokens.clone().into_iter()), tokens);
+    }
+    #[test]
+    fn test_peek() {
+        let t1 = token!(1, Loc::head());
+        let t2 = token!(1, Loc::head());
+        let t3 = token!(1, Loc::head());
+        let mut tokens = TokenReader::new([t1.clone(), t2.clone(), t3.clone()].into_iter());
+        assert_eq!(tokens.peek(), Some(&t1));
+        assert_eq!(tokens.next(), Some(t1));
+        assert_eq!(tokens.peek(), Some(&t2));
+        assert_eq!(tokens.next(), Some(t2));
+        assert_eq!(tokens.peek(), Some(&t3));
+        assert_eq!(tokens.next(), Some(t3));
+        assert_eq!(tokens.peek(), None);
+        assert_eq!(tokens.next(), None);
     }
 }
