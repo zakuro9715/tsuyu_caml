c978ad9224af620126607ac4adf52d22add2c499
zakuro <z@kuro.red>
2022-06-19 21:21:42 +0900

    add define_with_params_and_init!

--- karaage_utils/src/lib.rs
+++ karaage_utils/src/lib.rs
@@ -11,3 +11,40 @@ mod option;
 pub use option::*;
 mod iter;
 pub use iter::*;
+
+#[macro_export]
+macro_rules! define_with_params_and_init {
+    (
+        $(
+            #[$attr:meta]
+        )*
+        $vis:vis struct $type:ty {
+            $( $field_vis:vis $field_name:ident : $field_type:ty , )+
+        }
+    ) => {
+        $crate::paste! {
+            $( #[$attr] )*
+            $vis struct $type {
+                $(
+                    $field_vis $field_name : $field_type,
+                )+
+            }
+
+            $vis struct [<$type InitParams>]{
+                $(
+                    pub $field_name : $field_type,
+                )+
+            }
+
+            impl $type {
+                pub fn init(params: [<$type InitParams>]) -> Self {
+                    Self {
+                        $(
+                            $field_name: params.$field_name,
+                        )+
+                    }
+                }
+            }
+        }
+    };
+}
